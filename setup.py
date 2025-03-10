7e1f010c8c6d31bdfc06f2ab6d65dfd1b362a72c
luli191 <chenhang.zheng.edu@outlook.com>
2025-03-10 15:16:05 +0800

    revise

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bayes-regression",
-    version="0.1.2",
+    version="0.1.3",
     packages=find_packages(),
     author="Chenhang Zheng",
     author_email="chenhang.zheng.edu@outlook.com",
